378cde988e45c9900e5d39bf74129cec66c55051
SilverWWW <wsilver222@gmail.com>
2025-05-03 17:04:18 -0700

    app

---
+++ ios-app/bike-share-widget/bike-share-widget/Settings/SettingsView.swift
@@ -0,0 +1,18 @@
+//
+//  SettingsView.swift
+//  bike-share-widget
+//
+//  Created by Will Silver on 5/2/25.
+//
+
+import SwiftUI
+
+struct SettingsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SettingsView()
+}
---
+++ ios-app/bike-share-widget/bike-share-widget/TabBarView.swift
@@ -0,0 +1,29 @@
+import SwiftUI
+
+struct TabBarView: View {
+    
+    var body: some View {
+        
+        TabView {
+            Group {
+                
+                HomeView()
+                    .tabItem {
+                        Image(systemName: "house.fill")
+                        Text("Home")
+                    }
+
+                SettingsView()
+                    .tabItem {
+                        Image(systemName: "gear")
+                        Text("Settings")
+                    }
+            }
+        }
+        .tint(Color.BSWMain)
+    }
+    
+}
+#Preview {
+    TabBarView()
+}
